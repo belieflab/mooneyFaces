fb4bc704ec4295281c91d87256f3290bca343bb1
belieflab <joshuagkenney@gmail.com>
2020-06-29 12:59:19 -0400

    added preload_images to jsPsych.init

--- index.php
+++ index.php
@@ -359,6 +359,7 @@ function saveData(name, data){
       jsPsych.init({
         timeline: timeline,
         show_progress_bar: true,
+        preload_images: [original_stimuli, inverted_stimuli],
         on_finish: function(){ saveData("mooney-faces_" + workerID, jsPsych.data.get().csv()); }
         //on_finish: function(){
           //jsPsych.data.get().filter([{test_part: 'test'},{test_part: 'prediction'},{test_part: 'c2_test'}]).localSave("csv", `test-self-deception-data.csv`);
