9d778364cbd2e10c2331c2f9e18fc185f5ccd0ae
joshkenney <joshuagkenney@gmail.com>
2020-09-24 17:21:06 -0400

    added index iterator

--- exp/timeline.js
+++ exp/timeline.js
@@ -42,6 +42,8 @@ let first_half = {
     data.interview_date = today;
     data.interview_age = ageAtAssessment;
     data.sex = sexAtBirth;
+    data.index = experimentIterator;
+    experimentIterator++;
     data.response = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(data.key_press);
     // data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
     if (data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response)){
@@ -71,6 +73,8 @@ let second_half = {
     data.interview_date = today;
     data.interview_age = ageAtAssessment;
     data.sex = sexAtBirth;
+    data.index = experimentIterator;
+    experimentIterator++;
     data.response = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(data.key_press)
     // data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
     if (data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response)){
