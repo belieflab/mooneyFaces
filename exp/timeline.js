196803f3007810a7121541a024b55dd221965ec0
joshkenney <joshuagkenney@gmail.com>
2020-09-30 11:47:14 -0400

    submitted data-definition.csv to NDA

--- exp/timeline.js
+++ exp/timeline.js
@@ -42,7 +42,7 @@ let first_half = {
     data.interview_date = today;
     data.interview_age = ageAtAssessment;
     data.sex = sexAtBirth;
-    data.index = experimentIterator;
+    data.trial = experimentIterator;
     experimentIterator++;
     data.response = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(data.key_press);
     // data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
@@ -73,7 +73,7 @@ let second_half = {
     data.interview_date = today;
     data.interview_age = ageAtAssessment;
     data.sex = sexAtBirth;
-    data.index = experimentIterator;
+    data.trial = experimentIterator;
     experimentIterator++;
     data.response = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(data.key_press)
     // data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
