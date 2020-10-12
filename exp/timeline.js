1b49da3183200cb442ae7138919103936f5a5c57
joshkenney <joshuagkenney@gmail.com>
2020-10-12 06:30:24 -0400

    ready for pilot

--- exp/timeline.js
+++ exp/timeline.js
@@ -120,7 +120,7 @@ let save_data = {
   choices: jsPsych.NO_KEYS,
   trial_duration: 5000,
   on_finish: function(){
-    saveData("delay-discounting_" + workerId, jsPsych.data.get().csv());
+    saveData("mooney-faces_" + workerId, jsPsych.data.get().csv());
   }
 };
 
