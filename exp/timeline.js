b38a3066269d799e93ce0f2c52100af31ea32695
Joshua Kenney <joshuagkenney@gmail.com>
2020-11-12 21:31:54 -0500

    ready for CAPR; new intake screens

--- exp/timeline.js
+++ exp/timeline.js
@@ -4,7 +4,7 @@ let timeline = [];
 /* define welcome message trial */
 let welcome = {
   type: "html-keyboard-response",
-  stimulus: '<h1 style="color:white;">Welcome to the experiment!</h1>'+
+  stimulus: '<h2 style="color:white;">Welcome to the experiment!</h2>'+
   '<p style="color:white;">Press any key to begin.</p>'
 };
 
@@ -12,9 +12,9 @@ let welcome = {
 /* define instructions trial */
 let instructions_1 = {
   type: "html-keyboard-response",
-  stimulus: '<h2 style="color:white;">At the begining of each trial, you will see a black and white image.</h2>' +
-    '<h3 style="color:white;">If you believe the image <u>is a face</u>, please press the <u>1</u> key on your keyboard.</h3>' +
-    '<h3 style="color:white;">If you believe the image <u>is not a face</u>, please press the <u>0</u> key on your keyboard.</h3>' +
+  stimulus: '<h3 style="color:white;">At the begining of each trial, you will see a black and white image.</h3>' +
+    '<h3 style="color:white;">If you believe the image is a <u>face</u>, please press the <u>1</u> key on your keyboard.</h3>' +
+    '<h3 style="color:white;">If you believe the image is <u>not a face</u>, please press the <u>0</u> key on your keyboard.</h3>' +
     '<p style="color:white;">Press either response keys to continue.</p>',
   choices: ['1', '0'],
 };
@@ -22,14 +22,27 @@ let instructions_1 = {
 
 let instructions_2 = {
   type: "html-keyboard-response",
-  stimulus: '<h2 style="color:white;">Please respond as quickly as possible while maintaining a high level of confidence in your choice.</h2>' +
+  stimulus: '<h3 style="color:white;">Please respond as quickly as possible while maintaining a high level of confidence in your choice.</h3>' +
       '<p style="color:white;">Press the spacebar when you are ready to begin the experiment.</p>',
   choices: [32],
 };
 
 let first_half = {
-  type: "image-keyboard-response",
-  stimulus: jsPsych.timelineVariable('stimulus'), //train_stimuli_array, //jsPsych.timelineVariable('stimulus'),
+  type: "html-keyboard-response",
+  stimulus: function(){
+    var html = "<img class='center' style='height: 225px; width: 225px; margin-left: 50px;' src='"+jsPsych.timelineVariable('stimulus', true)+"'>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<p style='color:white;'>Face (press 1)&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Not a Face (press 0)</p>";
+    // "<p style='color:white;'>Press 1 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Press 0</p>";
+    return html;
+  },
   choices: ['1', '0'],
   trial_duration: 5000,
   stimulus_height: 225,
@@ -38,9 +51,11 @@ let first_half = {
   on_finish: function(data){
     data.subject_key = 'GUID';
     data.src_subject_id = workerId;
+    data.site = siteNumber;
     data.interview_date = today;
     data.interview_age = ageAtAssessment;
     data.sex = sexAtBirth;
+    data.handedness = handedness;
     data.trial = experimentIterator;
     experimentIterator++;
     data.response = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(data.key_press);
@@ -58,8 +73,21 @@ let first_half = {
 
 
 let second_half = {
-  type: "image-keyboard-response",
-  stimulus: jsPsych.timelineVariable('stimulus'), //train_stimuli_array, //jsPsych.timelineVariable('stimulus'),
+  type: "html-keyboard-response",
+  stimulus: function(){
+    var html = "<img class='center' style='height: 225px; width: 225px; margin-left: 50px;' src='"+jsPsych.timelineVariable('stimulus', true)+"'>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<br>"+
+    "<p style='color:white;'>Face (press 1)&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Not a Face (press 0)</p>";
+    // "<p style='color:white;'>Press 1 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Press 0</p>";
+    return html;
+  },
   choices: ['1', '0'],
   trial_duration: 5000,
   stimulus_height: 225,
@@ -68,10 +96,12 @@ let second_half = {
   on_finish: function(data){
     data.subjectKey = 'GUID';
     data.src_subject_id = workerId;
+    data.site = siteNumber;
     data.interview_date = today;
     data.interview_age = ageAtAssessment;
     data.sex = sexAtBirth;
     data.trial = experimentIterator;
+    data.handedness = handedness;
     experimentIterator++;
     data.response = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(data.key_press)
     // data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
@@ -87,7 +117,7 @@ let second_half = {
 
 let fixation = {
   type: 'html-keyboard-response',
-  stimulus: '<div style="color:white; font-size:60px;">+</div>',
+  stimulus: '<div style="color:white; font-size:30px;">+</div>',
   choices: jsPsych.NO_KEYS,
   trial_duration: 1000,
   data: {test_part: 'fixation'}
@@ -95,7 +125,7 @@ let fixation = {
 
 let breaking = {
   type: 'html-keyboard-response',
-  stimulus: '<h2 style="color:white;">You are now on a halfway break.</h2>'+
+  stimulus: '<h3 style="color:white;">You are now on a halfway break.</h3>'+
   '<p style="color:white;">Press the spacebar when you are ready to continue.</p>',
   choices: [32],
   // trial_duration: 30000
@@ -103,7 +133,7 @@ let breaking = {
 
 let save_data = {
   type: "html-keyboard-response",
-  stimulus: "<p>Data saving...</p>"+
+  stimulus: "<p style='color:white;'>Data saving...</p>"+
   '<div class="sk-cube-grid">'+
 '<div class="sk-cube sk-cube1"></div>'+
 '<div class="sk-cube sk-cube2"></div>'+
@@ -115,7 +145,7 @@ let save_data = {
 '<div class="sk-cube sk-cube8"></div>'+
 '<div class="sk-cube sk-cube9"></div>'+
 '</div>'+
-  "<p>Do not close this window until the text dissapears.</p>",
+  "<p style='color:white;'>Do not close this window until the text dissapears.</p>",
 
   choices: jsPsych.NO_KEYS,
   trial_duration: 5000,
@@ -130,15 +160,49 @@ let save_data = {
 
 let end = {
   type: "html-keyboard-response",
-  stimulus:   "<p>Thank you!</p>"+
-  "<p>You have successfully completed the experiment and your data has been saved.</p>"+
-  "<p>To leave feedback on this task, please click the following link:</p>"+
+  stimulus:   "<p style='color:white'>Thank you!</p>"+
+  "<p style='color:white'>You have successfully completed the experiment and your data has been saved.</p>"+
+  "<p style='color:white'>To leave feedback on this task, please click the following link:</p>"+
   "<p style='color:white;'><a href="+feedbackLink+">Leave Task Feedback!</a></p>"+
   // "<p>Please wait for the experimenter to continue.</p>"+
-  "<p>You may now close the expriment window at anytime.</p>",
+  "<p style='color:white'>You may now close the expriment window at anytime.</p>",
   choices: jsPsych.NO_KEYS,
-  trial_duration: 60000,
 };
+
+// procedure
+
+let procedureInstructions = { //This loops over the object
+  timeline: [instructions_1, instructions_2], //if you put fixation in front and the feedback after, it will display those in that order
+  randomize_order: false,// This is the outer procedure, looping over the stimuli
+}
+
+let first_procedure = {
+  timeline: [fixation, first_half],
+  randomize_order: false,
+  timeline_variables: full_stim_shuffle.slice(0,43),
+  // randomize_order: true
+}
+
+let rest = {
+  timeline: [breaking],
+}
+
+let second_procedure = {
+  timeline: [fixation, second_half],
+  randomize_order: false,
+  timeline_variables: full_stim_shuffle.slice(43,86),
+  // randomize_order: true
+}
+
+timeline.push(welcome)
+timeline.push(procedureInstructions)
+timeline.push(first_procedure) //Object oriented.
+timeline.push(rest);
+timeline.push(second_procedure);
+timeline.push(save_data);
+timeline.push(end);
+
+
 
 
 
