7321f06a4dd00b2e8ba7974ed58203c26979178f
joshkenney <joshuagkenney@gmail.com>
2023-08-18 16:17:43 -0400

    modified db/validate.js exp/main.js and include/intake.php per Judy Thompson's requirements

--- exp/main.js
+++ exp/main.js
@@ -1,30 +1,36 @@
-
-let procedureInstructions = { //This loops over the object
-    timeline: [instructions_1, instructions_2, instructions_3, instructions_4, instructions_5], //if you put fixation in front and the feedback after, it will display those in that order
-    randomize_order: false// This is the outer procedure, looping over the stimuli
-  };
+let procedureInstructions = {
+    //This loops over the object
+    timeline: [
+        instructions_1,
+        instructions_2,
+        instructions_3,
+        instructions_4,
+        instructions_5,
+    ], //if you put fixation in front and the feedback after, it will display those in that order
+    randomize_order: false, // This is the outer procedure, looping over the stimuli
+};
 
 let first_procedure = {
-  timeline: [fixation, faces, if_node],
-  randomize_order: false,
-  timeline_variables: full_stim_shuffle.slice(0,53)
-  // randomize_order: true
+    timeline: [fixation, faces, if_node],
+    randomize_order: false,
+    timeline_variables: full_stim_shuffle.slice(0, 53),
+    // randomize_order: true
 };
 
 let rest = {
-  timeline: [breaking]
+    timeline: [breaking],
 };
 
 let second_procedure = {
-  timeline: [fixation, faces, if_node],
-  randomize_order: false,
-  timeline_variables: full_stim_shuffle.slice(53,106),
+    timeline: [fixation, faces, if_node],
+    randomize_order: false,
+    timeline_variables: full_stim_shuffle.slice(53, 106),
 };
 
-timeline.push(welcome)
-timeline.push(procedureInstructions)
-timeline.push(first_procedure) //Object oriented.
-timeline.push(rest);
-timeline.push(second_procedure);
+timeline.push(welcome);
+// timeline.push(procedureInstructions)
+// timeline.push(first_procedure) //Object oriented.
+// timeline.push(rest);
+// timeline.push(second_procedure);
 timeline.push(save_data);
 timeline.push(end);
